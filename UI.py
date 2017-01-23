170247b11c1a120f12b2733181dead554c3fd1a5
Ken Roberts <kr26633z@gmail.com>
2017-01-23 23:22:07 +0000

    timer working but need to figure out how to update label to new value - mixed thread error

--- UI.py
+++ UI.py
@@ -1,36 +1,54 @@
 
-from PyQt5.QtWidgets import (QWidget, QPushButton,
-                             QHBoxLayout, QVBoxLayout, QApplication, QLabel, QLineEdit)
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QFont
+import threading
+
+
 
 class Interface(QWidget):
     def __init__(self):
         super().__init__()
-        print("in class")
+        self.time_elapsed = 0
+
         self.init_ui()
+        self.show()
+        self.run_timer()
     
     def init_ui(self):
+
+        self.resize(400, 200)
+        button_x_pos = 160
+        button_y_pos = 150
+        button_x_diff = 70
+        button_y_diff = 0
+        self.timer_label_text = "Default"
 
         start_button = QPushButton("Start", self)
         stop_button = QPushButton("Stop", self)
         close_button = QPushButton("Close", self)
-        
-        hbox = QHBoxLayout()
-        hbox.addStretch(1)
-        hbox.addWidget(start_button)
-        hbox.addWidget(stop_button)
-        hbox.addWidget(close_button)
-        
-        vbox = QVBoxLayout()
-        vbox.addStretch(1)
-        vbox.addLayout(hbox)
-        
-        self.setLayout(vbox)
-        self.resize(400,200)
+        timer_label = QLabel(self.timer_label_text, self)
+        timer_label.setFont(QFont('SansSerif', 40))
 
-        self.show()
+        start_button.move(button_x_pos, button_y_pos)
+        stop_button.move(start_button.pos().x() + button_x_diff, start_button.pos().y() + button_y_diff)
+        close_button.move(stop_button.pos().x() + button_x_diff, stop_button.pos().y() + button_y_diff)
+        timer_label.move(50, 50)
 
-# app = QApplication(sys.argv)
-#
-# window = Interface()
-# sys.exit(app.exec_())
+    def run_timer(self):
+        t = threading.Timer(1.0, self.inc_elapsed_time, [self.time_elapsed])
+        t.start()
+
+    def inc_elapsed_time(self, time_elapsed):
+        self.time_elapsed = time_elapsed
+        self.time_elapsed += 1
+        self.timer_label_text = self.time_elapsed
+        self.init_ui()
+        self.run_timer()
+
+
+
+
+
+
+
 
--- main.py
+++ main.py
@@ -1,13 +1,36 @@
 from UI import *
 import sys
-
+import threading
 
 def main():
     app = QApplication(sys.argv)
 
+    #create ui window
     timer_ui = Interface()
     sys.exit(app.exec_())
 
 if __name__ == "__main__":
     app = main()
     app.run()
+
+# class ThreadStopper(Thread):
+#
+#     def __init__(self):
+#         Thread.__init__()
+#         self.stop_event = Event()
+#
+#     def stop(self):
+#         if self.isAlive() == True:
+#             self.stop_event.set()
+#             self.join()
+#
+# class IntervalTimer(ThreadStopper):
+#     def __init__(self, interval, func):
+#         super().__init__()
+#         self.interval = interval
+#         self.func = func
+#
+#     def run(self):
+#         while not self.stop_event.is_set():
+#             self._func()
+#             sleep(self._interval)
