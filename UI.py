2127e67016a36ecc0d0dfbfe9b90caa1dd91b558
DaedalusKR <kr26633z@gmail.com>
2017-01-23 21:11:21 +0000

    UI working from main

--- UI.py
+++ UI.py
@@ -1,25 +1,36 @@
-import sys
-from QWidgit import *
 
-class Interface(self):
-    def __init__.(self)
-        super.__init__()
+from PyQt5.QtWidgets import (QWidget, QPushButton,
+                             QHBoxLayout, QVBoxLayout, QApplication, QLabel, QLineEdit)
+
+class Interface(QWidget):
+    def __init__(self):
+        super().__init__()
+        print("in class")
         self.init_ui()
     
     def init_ui(self):
-        
+
         start_button = QPushButton("Start", self)
         stop_button = QPushButton("Stop", self)
         close_button = QPushButton("Close", self)
         
         hbox = QHBoxLayout()
         hbox.addStretch(1)
-        hbox.addLayout(start_button)
-        hbox.addLayout(end_button)
-        hbox.addLayout(close_button)
+        hbox.addWidget(start_button)
+        hbox.addWidget(stop_button)
+        hbox.addWidget(close_button)
         
         vbox = QVBoxLayout()
         vbox.addStretch(1)
         vbox.addLayout(hbox)
         
-        self.addLayout(vbox)
+        self.setLayout(vbox)
+        self.resize(400,200)
+
+        self.show()
+
+# app = QApplication(sys.argv)
+#
+# window = Interface()
+# sys.exit(app.exec_())
+
--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from UI import *
+import sys
 
+
+def main():
+    app = QApplication(sys.argv)
+
+    timer_ui = Interface()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    app = main()
+    app.run()
