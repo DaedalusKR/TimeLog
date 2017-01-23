f876da2e0f938a221b485604d4c741ff9d6c66b0
DaedalusKR <kr26633z@gmail.com>
2017-01-23 13:40:24 +0000

    Update UI.py

--- UI.py
+++ UI.py
@@ -1,1 +1,25 @@
+import sys
+from QWidgit import *
 
+class Interface(self):
+    def __init__.(self)
+        super.__init__()
+        self.init_ui()
+    
+    def init_ui(self):
+        
+        start_button = QPushButton("Start", self)
+        stop_button = QPushButton("Stop", self)
+        close_button = QPushButton("Close", self)
+        
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addLayout(start_button)
+        hbox.addLayout(end_button)
+        hbox.addLayout(close_button)
+        
+        vbox = QVBoxLayout()
+        vbox.addStretch(1)
+        vbox.addLayout(hbox)
+        
+        self.addLayout(vbox)
