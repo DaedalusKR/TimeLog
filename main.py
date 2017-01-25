581bebf21907d860203d7cf280112bdaec2bd178
DaedalusKR <kr26633z@gmail.com>
2017-01-25 12:19:13 +0000

    Update main.py
    cleanup

--- main.py
+++ main.py
@@ -9,32 +9,11 @@ def main():
     app = QApplication(sys.argv)
 
     #create ui window
-
     timer_ui = Interface()
+
+    #ensure system exits cleanly when main terminates
     sys.exit(app.exec_())
 
 if __name__ == "__main__":
     app = main()
     app.run()
-
-# class ThreadStopper(Thread):
-#
-#     def __init__(self):
-#         Thread.__init__()
-#         self.stop_event = Event()
-#
-#     def stop(self):
-#         if self.isAlive() == True:
-#             self.stop_event.set()
-#             self.join()
-#
-# class IntervalTimer(ThreadStopper):
-#     def __init__(self, interval, func):
-#         super().__init__()
-#         self.interval = interval
-#         self.func = func
-#
-#     def run(self):
-#         while not self.stop_event.is_set():
-#             self._func()
-#             sleep(self._interval)
